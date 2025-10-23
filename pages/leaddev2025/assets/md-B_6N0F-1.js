6a553d42f9b4794d5ccec5f1ab2b0c0058bb24a6
Maciej Rzasa <maciejrzasa@gmail.com>
2025-10-23 09:31:12 +0200

    new version of Lead Dev 2025 slides

---
+++ pages/leaddev2025/assets/md-B_6N0F-1.js
@@ -0,0 +1,18 @@
+import{b as r,o as i,w as l,i as m,g as t,ad as s,v as p,x as u,T as n}from"./modules/vue-BcnDNB_i.js";import{_ as h}from"./slidev/cover.vue_vue_type_script_setup_true_lang-BYRVjHsz.js";import{u as d,f}from"./slidev/context-BXf1e0-e.js";import"./index-CynJRAB6.js";import"./modules/shiki-23BuDljT.js";const c="/pages/leaddev2025/img/chattermill-white.png",T={__name:"slides.md__slidev_1",setup(_){const{$clicksContext:a,$frontmatter:o}=d();return a.setup(),(g,e)=>(i(),r(h,p(u(n(f)(n(o),0))),{default:l(()=>[m(`
+/* Change the font */
+:root {
+  --slidev-font: 'Poppins', sans-serif;
+  /* Change the main color */
+  --slidev-theme-primary: #291c45
+ /*#10c2ed*/
+}
+
+html, body {
+  font-family: var(--slidev-font) !important;
+}
+
+/* Ensure headings also use the same font */
+h1, h2, h3, h4, h5, h6, p, span, li,  {
+  font-family: var(--slidev-font) !important;
+}
+`),e[0]||(e[0]=t("div",{class:"bg-black/20 pt-5 pb-5"},[t("h1",null,"Machine Learning in Production."),t("h1",{class:"mt-10"}," The Hidden Effort"),t("h3",null,"Maciek Rząsa"),t("h3",null,"@mjrzasa"),t("img",{src:c,width:"300px",class:"ml-70 mt-5"})],-1)),e[1]||(e[1]=t("p",null,[t("small",{class:"absolute bottom-0 left-0 p-2 text-neutral-500",style:{"font-size":"10px"}},"STpcsDG")],-1)),e[2]||(e[2]=t("small",{class:"absolute bottom-0 right-0 p-2 text-neutral-500"},[s(" Photo by "),t("a",{href:"https://unsplash.com/@daminirathore?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"},"Damini"),s(" on "),t("a",{href:"https://unsplash.com/photos/a-large-building-with-a-fountain-in-front-of-it-dt_s3--VvSg?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"},"Unsplash")],-1))]),_:1},16))}};export{T as default};
